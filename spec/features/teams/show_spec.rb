238ab69231e8dced87a2db3baaaacc96b34ba301
Gavin Carew <gjcarew@gmail.com>
2022-08-26 17:25:46 -0700

    User story 10 finished

--- spec/features/teams/show_spec.rb
+++ spec/features/teams/show_spec.rb
@@ -3,25 +3,32 @@ require 'rails_helper'
 RSpec.describe 'teams show' do
   describe 'as a visitor' do
     describe 'When I visit /teams/:id' do
+      before :each do
+        @atos = Team.create!(name: "Atos", head_coach: "Andre Galvao", year_founded: 2008, is_brazilian: true)
+        @atos.competitors.create!(name: 'Alexandre de Jesus', weight_class: 88,previous_winner: false)
+        @atos.competitors.create!(name: 'Josh Hinger', weight_class: 88, previous_winner: false)
+        @atos.competitors.create!(name: 'Tye Ruotolo', weight_class: 88, previous_winner: false)
+        @atos.competitors.create!(name: 'Lucas Barbosa', weight_class: 88, previous_winner: false)
+      end
+
       it 'shows the team attributes' do
-        team = Team.create!(name: "Atos", head_coach: "Andre Galvao", year_founded: 2008, is_brazilian: false)
-        visit "/teams/#{team.id}"
-        expect(page).to have_content(team.name)
-        expect(page).to have_content(team.head_coach)
-        expect(page).to have_content(team.year_founded)
-        expect(page).to have_content("not from Brazil")
+        visit "/teams/#{@atos.id}"
+        expect(page).to have_content(@atos.name)
+        expect(page).to have_content(@atos.head_coach)
+        expect(page).to have_content(@atos.year_founded)
+        expect(page).to have_content("from Brazil")
       end
 
       it 'has a count of number of competitors' do
-        atos = Team.create!(name: "Atos", head_coach: "Andre Galvao", year_founded: 2008, is_brazilian: true)
-        atos.competitors.create!(name: 'Alexandre de Jesus', weight_class: 88,previous_winner: false)
-        atos.competitors.create!(name: 'Josh Hinger', weight_class: 88, previous_winner: false)
-        atos.competitors.create!(name: 'Tye Ruotolo', weight_class: 88, previous_winner: false)
-        atos.competitors.create!(name: 'Lucas Barbosa', weight_class: 88, previous_winner: false)
-        
-        visit "/teams/#{atos.id}"
+        visit "/teams/#{@atos.id}"
 
         expect(page).to have_content("Atos has 4 competitors at ADCC 2022")
+      end
+
+      it 'has a link to the team competitors index' do
+        visit "/teams/#{@atos.id}"
+        click_link "#{@atos.name} competitors"
+        expect(current_path).to eq("/teams/#{@atos.id}/competitors")
       end
     end
   end
