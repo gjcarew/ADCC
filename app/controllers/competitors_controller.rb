4a60f5ccbf18cb1d953bf8efae44d93fee84f03f
Gavin Carew <gjcarew@gmail.com>
2022-08-29 15:25:12 -0700

    Complete user story 14: Child update

--- app/controllers/competitors_controller.rb
+++ app/controllers/competitors_controller.rb
@@ -7,4 +7,19 @@ class CompetitorsController < ApplicationController
     @competitor = Competitor.find(params[:id])
   end
 
+  def edit
+    @competitor = Competitor.find(params[:id])
+  end
+
+  def update
+    competitor = Competitor.find(params[:id])
+    competitor.update(competitor_params)
+    redirect_to "/competitors/#{params[:id]}"
+  end
+
+  private
+
+  def competitor_params
+    params.permit(:name, :weight_class, :previous_winner)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/teams', to: 'teams#index'
   get '/teams/new', to: 'teams#new'
-  get '/teams/:id', to: 'teams#show'
-  get '/teams/:id/edit', to: 'teams#edit'
-  patch '/teams/:id', to: 'teams#update'
   post '/teams', to: 'teams#create'
+  get '/teams/:id', to: 'teams#show'
+  patch '/teams/:id', to: 'teams#update'
+  get '/teams/:id/edit', to: 'teams#edit'
   get '/teams/:id/competitors', to: 'team_competitors#index'
   get '/teams/:id/competitors/new', to: 'team_competitors#new'
   post '/teams/:id/competitors', to: 'team_competitors#create'
   get '/competitors', to: 'competitors#index'
   get '/competitors/:id', to: 'competitors#show'
-  
-  
+  get '/competitors/:id/edit', to: 'competitors#edit'
+  patch '/competitors/:id', to: 'competitors#update'
 end
