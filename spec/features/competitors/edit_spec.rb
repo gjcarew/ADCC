b770ef48747aeba7e97152b102bddce395a0cb0b
Gavin Carew <gjcarew@gmail.com>
2022-08-29 19:17:06 -0700

    Complete User Story 18: Update child form child index and parent child index

--- spec/features/competitors/edit_spec.rb
+++ spec/features/competitors/edit_spec.rb
@@ -1,16 +1,3 @@
-# # [ ] done
-
-# # User Story 14, Child Update 
-
-# # As a visitor
-# # When I visit a Child Show page
-# # Then I see a link to update that Child "Update Child"
-# # When I click the link
-# # I am taken to '/child_table_name/:id/edit' where I see a form to edit the child's attributes:
-# # When I click the button to submit the form "Update Child"
-# # Then a `PATCH` request is sent to '/child_table_name/:id',
-# # the child's data is updated,
-# # and I am redirected to the Child Show page where I see the Child's updated information
 require 'rails_helper'
 
 RSpec.describe 'Update competitor', type: :feature do
@@ -43,6 +30,30 @@ RSpec.describe 'Update competitor', type: :feature do
       expect(page).to have_content('Gordon Ryan')
       expect(page).to have_content('99+')
       expect(page).to have_content('has won ADCC before')
+    end
+  end
+  
+  describe 'User story 18: child update from child index' do
+    before :each do
+      @team = Team.create!(name: "New Wave Jiu Jitsu",
+        head_coach: "John Danaher",
+        year_founded: 2021,
+        is_brazilian: false)
+      @competitor = @team.competitors.create!(name: "Gordon Ryan",
+                                weight_class: 100,
+                                previous_winner: true)
+    end
+
+    it 'There is a link to the edit page on the competitors index' do
+      visit '/competitors'
+      click_link "Edit #{@competitor.name}"
+      expect(current_path).to eq("/competitors/#{@competitor.id}/edit")
+    end
+
+    it 'There is a link to the edit page on the parent child index page' do
+      visit "/teams/#{@team.id}/competitors"
+      click_link "Edit #{@competitor.name}"
+      expect(current_path).to eq("/competitors/#{@competitor.id}/edit")
     end
   end
 end
